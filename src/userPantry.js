dad8fea6b9b6afce876fa994dea50079f414aee9
Anneke Miers <annekemiers.am@gmail.com>
2021-02-15 23:59:44 -0700

    Remove pseudocode

--- src/userPantry.js
+++ src/userPantry.js
@@ -12,7 +12,6 @@ class UserPantry {
 
   populateNeededIngredients(recipe) {
     this.neededIngredients = recipe.ingredients;
-    // console.log(this.neededIngredients)
   }
 
   populatePantryIngredientIDs() {
@@ -20,7 +19,6 @@ class UserPantry {
     this.pantry.forEach(item => {
       this.pantryIngredientIDs.push(item.ingredient);
     })
-    // console.log(this.pantryIngredientIDs);
   }
 
   compareIngredients(recipe) {
@@ -30,32 +28,6 @@ class UserPantry {
       }
     })
   }
-
-
-  // THIS WORKS
-  // compareIngredients() {
-  //   this.neededIngredients.forEach(neededIngredient => {
-  //     let array = [];
-  //     console.log(recipe.ingredients)
-  //     if (!this.pantryIngredientIDs.includes(neededIngredient.id)) {
-  //       // this.neededIngredients.splice(neededIngredient);
-  //       array.push(neededIngredient)
-  //       // console.log(array)
-  //     }
-  //   })
-  //   // console.log(this.neededIngredients)
-  //   // console.log(this.pantryIngredientIds)
-  // }
-
-  // input: comparing an array of objects to an array of objects
-  //for each neededIngredient, go through user's pantry
-  //if neededIngredient instance.id === user pantry.ingredient(it's a number), slice
-  //for each ingredient in the recipe, loop through the user's pantry by id.
-  // once the match is found - if recipe.ingredients.amount is greater than the user's pantry, return "No"
-  //else if the amount is = or less than what's in user's pantry, return "yes"
-
-  // if no match is found, return Hellz no 
-
 
   useIngredients() {
     //for each ingredient
